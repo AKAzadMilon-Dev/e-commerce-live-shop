f1330074aaed039b6b16c836908a8c4739f5514b
AK Azad Milon <akazadmilon.dev@gmail.com>
2023-01-26 23:05:51 +0600

    update

--- frontend/src/components/admin/AdminRoleManagement.js
+++ frontend/src/components/admin/AdminRoleManagement.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Col, Row, Tabs, Tab, Form, Button } from 'react-bootstrap';
+import { Col, Row, Tabs, Tab, Form, Button, Table } from 'react-bootstrap';
 import AdminNavbar from './AdminNavbar';
 
 const AdminRoleManagement = () => {
@@ -104,7 +104,35 @@ const AdminRoleManagement = () => {
                 <h3 className='welcome'>Role Assign List</h3>
                 <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
                     <Tab eventKey="rolelist" title="Role List">
-                        Role List
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                </tr>
+                                <tr>
+                                <td>3</td>
+                                <td colSpan={2}>Larry the Bird</td>
+                                <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </Table>
                     </Tab>
                     <Tab eventKey="assignrole" title="Assign Role">
                         <Form>
